4bb1beeaaaae43f9fc90796ee73716049b7deffa
Nikesh <nikeshwarpandey@gmail.com>
2018-03-11 21:20:00 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const API_KEY = "AIzaSyCbvNLy4err3GTyDDLv59QkshdQboa3zz8";
+
+const App = function(){
+  return <div> Hello Motto </div>;
+}
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
